4c79d5f69be95dae779edebd031272b8299dfa89
AdvaitVuppalanchi13 <74249379+AdvaitVuppalanchi13@users.noreply.github.com>
2020-11-22 15:26:28 +0530

    Add files via upload

---
+++ Screens/WriteStoryScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+
+export default class WriteStoryScreen extends React.Component {
+
+}
