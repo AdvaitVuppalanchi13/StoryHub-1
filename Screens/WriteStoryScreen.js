7b5791d518b412c3373d16482eb357478fd50d33
AdvaitVuppalanchi13 <74249379+AdvaitVuppalanchi13@users.noreply.github.com>
2020-12-01 18:36:46 +0530

    Added header and text input

--- Screens/WriteStoryScreen.js
+++ Screens/WriteStoryScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import {TextInput} from 'react-native-gesture-handler'
+import {Header} from 'react-native-elements'
 
 
 export default class WriteStoryScreen extends React.Component {
